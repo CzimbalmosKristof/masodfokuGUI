3efbfda1f76f07ce34377e721ea8b214469dd2ba
czimbalmoskristof23 <czimbalmoskristof23@Kalmar-16>
2024-11-27 19:14:50 +0100

    Add project files.

---
+++ MasodfokuGUI/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MasodfokuGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ellenorzogomb = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(38, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(215, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 29);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(401, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 29);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(129, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "x^2+";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(316, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 27);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "x+";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(503, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 27);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "=0";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(86, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 27);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Adja meg az egyenlet együtthatóit!";
+            // 
+            // ellenorzogomb
+            // 
+            this.ellenorzogomb.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ellenorzogomb.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ellenorzogomb.Location = new System.Drawing.Point(115, 155);
+            this.ellenorzogomb.Name = "ellenorzogomb";
+            this.ellenorzogomb.Size = new System.Drawing.Size(342, 38);
+            this.ellenorzogomb.TabIndex = 10;
+            this.ellenorzogomb.Text = "Ellenőríz és számol";
+            this.ellenorzogomb.UseVisualStyleBackColor = false;
+            this.ellenorzogomb.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(74, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(425, 33);
+            this.label4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Gainsboro;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(74, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(425, 33);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "x1=";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.LightGray;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(74, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(425, 33);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "x2=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ellenorzogomb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ellenorzogomb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MasodfokuGUI/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasodfokuGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a;
+                double b;
+                double c;
+
+                try
+                {
+                    a = double.Parse(textBox1.Text);
+                }
+                catch (Exception)
+                {
+
+                    throw new Exception("Nem számot adott meg az 1. együtthatóként!");
+                }
+                if (!double.TryParse(textBox2.Text, out b) ) 
+                {
+                    throw new Exception("Nem  számot adott meg az 2. együtthatóként!");
+                }
+                if (!double.TryParse(textBox3.Text, out c) ) 
+                {
+                    throw new Exception("Nem  számot adott meg az 3. együtthatóként!");
+                }
+                if(a==0)
+                {
+                    throw new Exception("Az első együttható nulla!");
+                }
+
+                double d = b * b - 4 * a * c;
+                if (d < 0)
+                {
+                    throw new Exception("Az egyenletnek nincs valós gyöke (diszkr.=0)!");
+                }
+                if(d>0)
+                {
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    label7.Text = $"x2 ={x2}";
+                }
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    label6.Text = $"x1 ={x1}";
+
+            }
+            catch (Exception hiba)
+            {
+
+                label4.Text =hiba.Message;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            label6.Text = "x1=";
+            label7.Text = "x2=";
+        }
+    }
+}
